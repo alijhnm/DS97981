f9c1c3e075f579403919136d7592f7c7f2dc6d9a
Ali Jahangiri <alijahangiri.m@gmail.com>
2018-09-27 11:56:41 +0330

    test files renamed and changed. All tests passed on PC

--- A1/A1Tests/Properties/AssemblyInfo.cs
+++ A1/A1Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("A1Tests2")]
+[assembly: AssemblyTitle("A1Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("A1Tests2")]
+[assembly: AssemblyProduct("A1Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8046f9ea-a630-46de-84d5-d8e2339cb85a")]
+[assembly: Guid("60dbf5af-6cff-415c-825d-feb81eadb385")]
 
 // Version information for an assembly consists of the following four values:
 //
