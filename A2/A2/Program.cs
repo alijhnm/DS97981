51f5fdd815b9731368316de8230821a11446c481
Ali Jahangiri <alijahangiri.m@gmail.com>
2018-10-05 00:02:41 +0330

    A2 done!

---
+++ A2/A2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static string Process(String input)
+        {
+            var inData = input.Split(new char[] { '\n', '\r', ' ' },
+                StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToList();
+
+            return FastMaxPairwiseProduct(inData).ToString();
+
+        }
+        public static int NaiveMaxPairwiseProduct(List<int> list)
+        {
+            int result = 0;
+            for (int index1 = 0; index1 < list.Count; index1++)
+            {
+                for (int index2 = index1 + 1; index2 < list.Count; index2++)
+                {
+                    if (list[index1] * list[index2] > result)
+                    {
+                        result = list[index1] * list[index2];
+                    }
+                }
+            }
+            return result;
+        }
+        public static int FastMaxPairwiseProduct(List<int> list)
+        {
+            int firstIndex = 0;
+            for (int index = 0; index < list.Count; index++)
+            {
+                if (list[index] > list[firstIndex])
+                {
+                    firstIndex = index;
+                }
+            }
+            int secondIndex;
+            if (firstIndex == 0) {
+                secondIndex = 1;
+            }
+            else
+            {
+                secondIndex = 0;
+            }
+            
+
+            for(int index = 0; index < list.Count; index++)
+            {
+                if (index != firstIndex && list[index] > list[secondIndex])
+                {
+                    secondIndex = index;
+                }
+            }
+            return list[firstIndex] * list[secondIndex];
+        }
+    }
+}
---
+++ A2/A2Tests/ProgramTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace A2.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        [DeploymentItem("TestData", "TestData")]
+        public void GradedTest_Correctness()
+        {
+            TestCommon.TestTools.RunLocalTest(Program.Process);
+        }
+        [TestMethod(), Timeout(500)]
+        [DeploymentItem("TestData", "TestData")]
+        public void GradedTest_Performance()
+        {
+            TestCommon.TestTools.RunLocalTest(Program.Process);
+        }
+        [TestMethod()]
+        public void GradedTest_Stress()
+        {
+            int maximumListSize = 10;
+            int M = 10;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            while (sw.ElapsedMilliseconds < 5000)
+            {
+                Random randomGenerator = new Random();
+                int listSize = randomGenerator.Next(2, maximumListSize);
+                List<int> list = new List<int>();
+                for (int i = 0; i < listSize; i++)
+                {
+                    list.Add(randomGenerator.Next(0, M));
+                }
+                int naiveResult = Program.NaiveMaxPairwiseProduct(list);
+                int fastResult = Program.FastMaxPairwiseProduct(list);
+                if (naiveResult == fastResult)
+                {
+                    Console.WriteLine("OK");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong answer: ", naiveResult.ToString(), fastResult.ToString());
+                }
+            }
+        }
+    }
+}
