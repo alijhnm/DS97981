074dffbf4b7848b3450b68cb3ab751d657924b71
Ali jahangirim <alijahangiri.m@gmail.com>
2018-12-17 23:06:55 +0330

    A10 Done

---
+++ A10/A10Tests/GradedTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A10;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A10.Tests
+{
+    [TestClass()]
+    public class GradedTests
+    {
+        [TestMethod(), Timeout(10000)]
+        [DeploymentItem("TestData", "A10_TestData")]
+        public void SolveTest()
+        {
+            Processor[] problems = new Processor[] {
+                new PhoneBook("TD1"),
+                new HashingWithChain("TD2"),
+                new RabinKarp("TD3")
+            };
+
+            foreach (var p in problems)
+            {
+                TestTools.RunLocalTest("A10", p.Process, p.TestDataName);
+            }
+        }
+    }
+}
---
+++ A10/A10Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("A10Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("A10Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("66e7215c-f265-4aba-97df-d70882894d97")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
